6b6248feddd357c5f679ebac6bb3faedb0945dea
Kiwii Vanya <kiwiivanya128@gmail.com>
2023-08-19 21:17:59 +0300

    set up dependencies and of course raylib

--- Evolv.io.wtk/src/main.cpp
+++ Evolv.io.wtk/src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include "raylib.h"
+
 int main() {
-    std::cout << "Are you okay?\n";
+    InitWindow(640, 480, "Evolv.io.wtk");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawText("this text should show for me!", 100, 100, 24, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Evolv.io.wtk
+This is a first solution what will using base from WanichusilkToolKit
+
+It was inspired by a project called 'Evolv.io' by carykh.
+Basic things will implemented in C++ using RayLib and of course WanichusilkToolKit
+
